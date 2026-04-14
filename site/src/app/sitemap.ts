92728f42695bbb3bf29fda763ec53d36dcf62e7a
Liiift <hello@liiift.studio>
2026-04-14 04:11:10 -0700

    v0.0.8

--- site/src/app/sitemap.ts
+++ site/src/app/sitemap.ts
@@ -5,7 +5,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'https://speechtype.liiift-studio.com',
+			url: 'https://speechtype.vercel.app',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 1,
